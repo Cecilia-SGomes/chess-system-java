ec5d8f38fad1fe01ac45c64cbf97a98799938a11
Cecilia-SGomes <cecilia.gomes@academico.ifpb.edu.br>
2025-06-01 14:33:50 -0300

    Bishop

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -25,7 +25,7 @@ public class Pawn extends ChessPiece {
             p.setValues(position.getRow() - 2, position.getColumn());
             Position p2 = new Position(position.getRow() - 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
-                    && getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+                    && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
             p.setValues(position.getRow() - 1, position.getColumn() - 1);
@@ -36,6 +36,7 @@ public class Pawn extends ChessPiece {
             if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
+
         } else {
             p.setValues(position.getRow() + 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
@@ -44,7 +45,7 @@ public class Pawn extends ChessPiece {
             p.setValues(position.getRow() + 2, position.getColumn());
             Position p2 = new Position(position.getRow() + 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
-                    && getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+                    && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
             p.setValues(position.getRow() + 1, position.getColumn() - 1);
@@ -55,7 +56,6 @@ public class Pawn extends ChessPiece {
             if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
-
         }
         return mat;
     }
